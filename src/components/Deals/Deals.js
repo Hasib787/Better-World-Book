ec5c10ec90c0c4937ceff6880dfa28512d70bf91
Hasib787 <mr.hasib45@gmail.com>
2021-04-04 00:03:36 +0600

    added some components

---
+++ src/components/Deals/Deals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Deals = () => {
+    return (
+        <div>
+            <h2>The page is coming soon....</h2>
+        </div>
+    );
+};
+
+export default Deals;
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Orders = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <h1>This is Order</h1>
         </div>
     );
 };

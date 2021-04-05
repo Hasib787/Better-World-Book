f1821603dab05401417f98cfc8348d088c58fc23
Hasib787 <mr.hasib45@gmail.com>
2021-04-05 12:12:57 +0600

    added some component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+export const initializeLoginFramework = () => {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app(); // if already initialized, use that one
+    }
+}
 
 const Login = () => {
-    
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogleSignin=()=>{
+        firebase.auth()
+        .signInWithPopup(googleProvider)
+        .then((result) => {
+            const credential = result.credential;
+            const token = credential.accessToken;
+            var user = result.user;
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+            console.log(errorCode, errorMessage, email, credential);
+        });
+
+    }
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div >
+            <button className={handleGoogleSignin}>Sign In</button>
         </div>
     );
 };

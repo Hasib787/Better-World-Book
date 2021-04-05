f47324c7a08aa966d0325095e71389d70fed022a
Hasib787 <mr.hasib45@gmail.com>
2021-04-05 16:51:52 +0600

    added some design on admin page

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import './Admin.css'
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 const Admin = () => {
+    const handleAddBook = () => {
+
+    }
+    const handleImageUpload = (event) => {
+        console.log(event.target.files)
+    }
     return (
         <div>
-            <h1>This is admin</h1>
+            <Row>
+                <Col id="sidebar" xs={3}>
+                    <h2>Book Shop</h2>
+                </Col>
+                <Col xs={9}>
+                    <h4>Add book</h4>
+                    <Col id="addBook">
+                        <Col id="inputField">
+                            <p>Book Name</p>
+                            <input type="text" placeholder="Enter Name" />
+                            <p>Author Name </p>
+                            <input type="text" placeholder="Enter Name" />
+                            <br />
+                            Add Price
+                            <input type="text" placeholder="Enter Price" />
+                            Add Book Cover Photo
+                            <input type="file" name="Uploaded photo" onChange={handleImageUpload} />
+                            <Button onClick={handleAddBook}>Save</Button>
+                        </Col>
+                    </Col>
+                </Col>
+            </Row>
+
         </div>
     );
+
+    // <h2>Add book</h2>
+    //         <div className="side-bar">
+
+    //         </div>
+    //         <div className="addBooks">
+    //             <p>Book Name</p>
+    //             <input type="text" placeholder="Enter Name"/>
+    //             <p>Author Name </p>
+    //             <input type="text" placeholder="Enter Name"/>
+    //             <br/>
+    //             Add Price
+    //             <input type="text" placeholder="Enter Price"/>
+    //             Add Book Cover Photo
+    //             <input type="file" name="Uploaded photo"/>
+    //         </div>
 };
 
 export default Admin;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from './firebase.config';
+// import firebase from "firebase/app";
+// import "firebase/auth";
+// import firebaseConfig from './firebase.config';
 
-export const initializeLoginFramework = () => {
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    } else {
-        firebase.app(); // if already initialized, use that one
-    }
-}
+// export const initializeLoginFramework = () => {
+//     if (!firebase.apps.length) {
+//         firebase.initializeApp(firebaseConfig);
+//     } else {
+//         firebase.app(); // if already initialized, use that one
+//     }
+// }
 
 const Login = () => {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    // const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-    const handleGoogleSignin=()=>{
-        firebase.auth()
-        .signInWithPopup(googleProvider)
-        .then((result) => {
-            const credential = result.credential;
-            const token = credential.accessToken;
-            var user = result.user;
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-            console.log(errorCode, errorMessage, email, credential);
-        });
-
+    const handleGoogleSignin = () => {
+        // firebase.auth()
+        // .signInWithPopup(googleProvider)
+        // .then((result) => {
+        //     const credential = result.credential;
+        //     const token = credential.accessToken;
+        //     var user = result.user;
+        // })
+        // .catch((error) => {
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     var email = error.email;
+        //     var credential = error.credential;
+        //     console.log(errorCode, errorMessage, email, credential);
+        // });
+        console.log("sign in is clicked")
     }
     return (
         <div >
-            <button className={handleGoogleSignin}>Sign In</button>
+            <button onClick={handleGoogleSignin}>Sign In</button>
         </div>
     );
 };

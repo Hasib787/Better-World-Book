ab6385e0ebf9641de3acde128cee709e2d9a6533
Hasib787 <mr.hasib45@gmail.com>
2021-04-10 21:02:18 +0600

    insert data to the database

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -4,22 +4,25 @@ import axios from 'axios';
 import Admin from '../Admin/Admin';
 import './AddBook.css';
 import { useForm } from 'react-hook-form';
-const AddBook = () => {
-    const { handleSubmit } = useForm()
-    
-    const [imageUrl, setImageUrl] = useState('');
 
-    const handleAddBook = (data) => {
-        console.log(data.name);
+const AddBook = () => {
+    const { register, handleSubmit } = useForm()
+
+    const [imageUrl, setImageUrl] = useState(null);
+
+    const onSubmit = data => {
+        console.log(data);
         const eventData = {
-            name: data.name,
+            bookName: data.bookName,
+            authorName: data.authorName,
+            addPrice: data.addPrice,
             imageUrl: imageUrl
         }
         console.log(eventData);
         const url = `http://localhost:5000/addBooks`;
         fetch(url, {
             method: 'POST',
-            headers: { 'content-type': 'application/json' },
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(eventData)
         })
             .then(res => console.log('server site response'))
@@ -41,33 +44,34 @@ const AddBook = () => {
         <div>
             <Admin></Admin>
             <div className="addbooks">
-                <Form onSubmit={handleSubmit(handleAddBook)}>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Row>
                         <Form.Group as={Col} controlId="text">
-                            <Form.Label className="text-label">Book Name</Form.Label>
-                            <Form.Control name="bookName"  type="text" placeholder="Book Name" />
+                            <Form.Label className="text-label" >Book Name</Form.Label>
+                            <Form.Control name="bookName"  {...register("bookName")} type="text" placeholder="Book Name" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="text">
                             <Form.Label className="text-label">Author Name</Form.Label>
-                            <Form.Control name="authorName" type="text" placeholder="Author Name" />
+                            <Form.Control name="authorName" {...register("authorName")} type="text" placeholder="Author Name" />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
                         <Form.Group as={Col} controlId="text">
                             <Form.Label className="text-label">Add Price</Form.Label>
-                            <Form.Control name="addPrice" type="number" placeholder="Enter Price" />
+                            <Form.Control name="addPrice" {...register("addPrice")} type="number" placeholder="Enter Price" />
                         </Form.Group>
 
                         <Form.Group>
                             <Form.File className="text-label" onChange={handleImageUpload} id="exampleFormControlFile1" label="Add Book Cover Photo" />
                         </Form.Group>
                     </Form.Row>
+                    <div id="saveButton">
+                        <Button type="submit">Save</Button>
+                    </div>
                 </Form>
             </div>
-            <div id="saveButton">
-                <Button onClick={handleAddBook}>Save</Button>
-            </div>
+           
         </div>
     );
 };
